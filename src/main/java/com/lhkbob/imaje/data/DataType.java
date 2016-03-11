60c5784b28438c879724a50363d807dbec2db4d2
Michael Ludwig <lhkbob@gmail.com>
2016-03-10 22:37:22 -0600

    Adding missing enum file.

---
+++ src/main/java/com/lhkbob/imaje/data/DataType.java
@@ -0,0 +1,12 @@
+package com.lhkbob.imaje.data;
+
+/**
+ *
+ */
+public enum DataType {
+  UINT8, UINT16, UINT32, UINT64,
+  SINT8, SINT16, SINT32, SINT64,
+  UFIXED8, UFIXED16, UFIXED32, UFIXED64,
+  SFIXED8, SFIXED16, SFIXED32, SFIXED64,
+  FLOAT16, FLOAT32, FLOAT64
+}
