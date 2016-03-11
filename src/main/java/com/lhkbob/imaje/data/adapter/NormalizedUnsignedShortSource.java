89007cebcc9f5917d5899de976ac7513f5738083
Michael Ludwig <lhkbob@gmail.com>
2016-03-10 22:40:20 -0600

    Update to nuanced type system. Fix normalization clamping range from [-1,1] to [0,1] given that these are the unsigned variants.

--- src/main/java/com/lhkbob/imaje/data/adapter/NormalizedUnsignedShortSource.java
+++ src/main/java/com/lhkbob/imaje/data/adapter/NormalizedUnsignedShortSource.java
@@ -1,5 +1,6 @@
 package com.lhkbob.imaje.data.adapter;
 
+import com.lhkbob.imaje.data.DataType;
 import com.lhkbob.imaje.data.DataView;
 import com.lhkbob.imaje.data.DoubleSource;
 
@@ -28,8 +29,23 @@ public class NormalizedUnsignedShortSource implements DoubleSource, DataView<Uns
   }
 
   @Override
+  public boolean isBigEndian() {
+    return source.isBigEndian();
+  }
+
+  @Override
+  public DataType getDataType() {
+    return DataType.UFIXED16;
+  }
+
+  @Override
+  public boolean isGPUAccessible() {
+    return source.isGPUAccessible();
+  }
+
+  @Override
   public void set(long index, double value) {
-    source.set(index, (int) (TO_BYTE_SCALAR * Math.max(-1.0, Math.min(value, 1.0))));
+    source.set(index, (int) (TO_BYTE_SCALAR * Math.max(0.0, Math.min(value, 1.0))));
   }
   private static final double TO_BYTE_SCALAR = (double) Short.MAX_VALUE - (double) Short.MIN_VALUE;
   private static final double TO_DOUBLE_SCALAR = 1.0 / TO_BYTE_SCALAR;
