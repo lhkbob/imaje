ad8286a4795c20f2edaf6b07120a7263d31f6677
Michael Ludwig <lhkbob@gmail.com>
2016-04-08 20:02:33 -0500

    Add clamping utility functions to avoid writing nested min/max calls.

---
+++ src/main/java/com/lhkbob/imaje/util/Functions.java
@@ -0,0 +1,35 @@
+package com.lhkbob.imaje.util;
+
+/**
+ *
+ */
+public final class Functions {
+  private Functions() {}
+
+  public static double clamp(double value, double min, double max) {
+    if (value > max)
+      return max;
+    else if (value < min)
+      return min;
+    else
+      return value;
+  }
+
+  public static long clamp(long value, long min, long max) {
+    if (value > max)
+      return max;
+    else if (value < min)
+      return min;
+    else
+      return value;
+  }
+
+  public static int clamp(int value, int min, int max) {
+    if (value > max)
+      return max;
+    else if (value < min)
+      return min;
+    else
+      return value;
+  }
+}
