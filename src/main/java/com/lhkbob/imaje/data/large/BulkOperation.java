d38064c85def87a47f3dc30ca4a0d48f2e15189b
Michael Ludwig <lhkbob@gmail.com>
2016-10-11 15:04:28 -0500

    Mark as a functional interface for clarity.

--- src/main/java/com/lhkbob/imaje/data/large/BulkOperation.java
+++ src/main/java/com/lhkbob/imaje/data/large/BulkOperation.java
@@ -3,6 +3,7 @@ package com.lhkbob.imaje.data.large;
 /**
  *
  */
+@FunctionalInterface
 public interface BulkOperation<S, D> {
   void run(S src, long srcOffset, D dst, int dstOffset, int length);
 }
