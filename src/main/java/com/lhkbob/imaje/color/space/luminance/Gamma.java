b2dd005b0a185f88ac34bd4e717b5fc179afcf01
Michael Ludwig <lhkbob@gmail.com>
2016-11-30 15:53:35 -0600

    Define and document linear and gamma-encoded luminance spaces.

---
+++ src/main/java/com/lhkbob/imaje/color/space/luminance/Gamma.java
@@ -0,0 +1,117 @@
+package com.lhkbob.imaje.color.space.luminance;
+
+import com.lhkbob.imaje.color.ColorSpace;
+import com.lhkbob.imaje.color.Luminance;
+import com.lhkbob.imaje.color.XYZ;
+import com.lhkbob.imaje.color.space.xyz.CIE31;
+import com.lhkbob.imaje.color.transform.ColorTransform;
+import com.lhkbob.imaje.color.transform.Illuminants;
+import com.lhkbob.imaje.color.transform.curves.Curve;
+import com.lhkbob.imaje.color.transform.curves.UnitGammaFunction;
+import com.lhkbob.imaje.color.transform.general.Composition;
+import com.lhkbob.imaje.color.transform.general.CurveTransform;
+
+import java.util.Collections;
+
+/**
+ * Gamma
+ * =====
+ *
+ * Gamma represents a Luminance color space that has been gamma encoded into non-linear compressed
+ * values. It is parameterized and defined relative to an XYZ whitepoint illuminant and has a
+ * function that decodes gamma-encoded values into linear luminance. Because values in this
+ * space are not in a linear coordinate system, they do not represent photometric values and
+ * must first be converted to a linear space.
+ *
+ * @author Michael Ludwig
+ */
+public class Gamma implements ColorSpace<Luminance<Gamma>, Gamma> {
+  /**
+   * A singleton instance representing luminance values encoded with the same gamma function
+   * and illuminant as the {@link com.lhkbob.imaje.color.space.rgb.SRGB} color space.
+   */
+  public static final Gamma SPACE_SRGB = new Gamma(Illuminants.newD65(1.0).toXYZ(),
+      new UnitGammaFunction(2.4, 1.0 / 1.055, 0.055 / 1.055, 0.0, 1.0 / 12.92, 0.0, 0.04045));
+
+  private final Linear baseSpace;
+  private final ColorTransform<Gamma, Luminance<Gamma>, Linear, Luminance<Linear>> gammaDecoder;
+  private final ColorTransform<Gamma, Luminance<Gamma>, CIE31, XYZ<CIE31>> toXYZ;
+
+  /**
+   * Create a new Gamma luminance color space that is defined relative to the given whitepoint,
+   * passed to {@link Linear#Linear(XYZ)} and represents values gamma-encoded such that
+   * `gammaDecoder` transforms the encoded values into linear luminance.
+   *
+   * @param referenceWhitepoint
+   *     The reference whitepoint
+   * @param gammaDecoder
+   *     The gamma decoding function
+   * @throws NullPointerException
+   *     if either argument is null
+   */
+  public Gamma(XYZ<CIE31> referenceWhitepoint, Curve gammaDecoder) {
+    baseSpace = new Linear(referenceWhitepoint);
+    this.gammaDecoder = new CurveTransform<>(
+        this, baseSpace, Collections.singletonList(gammaDecoder));
+    toXYZ = new Composition<>(this.gammaDecoder, baseSpace.getXYZTransform());
+  }
+
+  /**
+   * @return The underlying linear luminance space this depends on
+   */
+  public Linear getLinearSpace() {
+    return baseSpace;
+  }
+
+  /**
+   * @return The gamma decoding transformation from non-linear to linear space.
+   */
+  public ColorTransform<Gamma, Luminance<Gamma>, Linear, Luminance<Linear>> getGammaDecoder() {
+    return gammaDecoder;
+  }
+
+  @Override
+  public Luminance<Gamma> newColor() {
+    return new Luminance<>(this);
+  }
+
+  @Override
+  public ColorTransform<Gamma, Luminance<Gamma>, CIE31, XYZ<CIE31>> getXYZTransform() {
+    return toXYZ;
+  }
+
+  @Override
+  public int getChannelCount() {
+    return baseSpace.getChannelCount();
+  }
+
+  @Override
+  public String getChannelName(int channel) {
+    return baseSpace.getChannelName(channel);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    }
+    if (!(o instanceof Gamma)) {
+      return false;
+    }
+    Gamma g = (Gamma) o;
+    return g.baseSpace.equals(baseSpace) && g.gammaDecoder.equals(gammaDecoder);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = 17;
+    result = 31 * result + baseSpace.hashCode();
+    result = 31 * result + gammaDecoder.hashCode();
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Gamma-encoded Luminance<%s>", baseSpace.getReferenceWhitepoint());
+  }
+}
---
+++ src/main/java/com/lhkbob/imaje/color/space/luminance/Linear.java
@@ -0,0 +1,96 @@
+package com.lhkbob.imaje.color.space.luminance;
+
+import com.lhkbob.imaje.color.ColorSpace;
+import com.lhkbob.imaje.color.Luminance;
+import com.lhkbob.imaje.color.XYZ;
+import com.lhkbob.imaje.color.space.xyz.CIE31;
+import com.lhkbob.imaje.color.transform.Illuminants;
+import com.lhkbob.imaje.color.transform.LuminanceToXYZ;
+
+/**
+ * Linear
+ * ======
+ *
+ * A Luminance color space that is defined with respect to a white point illuminant. Like the RGB
+ * {@link com.lhkbob.imaje.color.space.rgb.Linear} color space, the luminance channel value is in a
+ * linear coordinate system. This means that values in the linear luminance space can be considered
+ * proper photometric quantities. As a parameterized color space, multiple instances of
+ * `Luminance<Linear>` may in fact refer to separate Linear spaces.
+ *
+ * @author Michael Ludwig
+ */
+public class Linear implements ColorSpace<Luminance<Linear>, Linear> {
+  /**
+   * Singleton instance for linear luminance relative to a D65 standard illuminant with luminance of
+   * 1.0.
+   */
+  public static final Linear SPACE_D65 = new Linear(Illuminants.newD65(1.0).toXYZ());
+
+  private final XYZ<CIE31> referenceWhitepoint;
+  private final LuminanceToXYZ toXYZ;
+
+  /**
+   * Create a new Linear luminance space that is relative to the given `referenceWhitepoint`.
+   *
+   * @param referenceWhitepoint
+   *     The reference whitepoint
+   * @throws NullPointerException
+   *     if `referenceWhitepoint` is null
+   */
+  public Linear(XYZ<CIE31> referenceWhitepoint) {
+    this.referenceWhitepoint = referenceWhitepoint;
+    toXYZ = new LuminanceToXYZ(this);
+  }
+
+  /**
+   * @return The reference whitepoint that this luminance is relative to
+   */
+  public XYZ<CIE31> getReferenceWhitepoint() {
+    return referenceWhitepoint.clone();
+  }
+
+  @Override
+  public Luminance<Linear> newColor() {
+    return new Luminance<>(this);
+  }
+
+  @Override
+  public LuminanceToXYZ getXYZTransform() {
+    return toXYZ;
+  }
+
+  @Override
+  public int getChannelCount() {
+    return 1;
+  }
+
+  @Override
+  public String getChannelName(int channel) {
+    if (channel == 0) {
+      return "Y";
+    } else {
+      throw new IndexOutOfBoundsException("Bad channel: " + channel);
+    }
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    }
+    if (!(o instanceof Linear)) {
+      return false;
+    }
+    return ((Linear) o).referenceWhitepoint.equals(referenceWhitepoint);
+  }
+
+  @Override
+  public int hashCode() {
+    return Linear.class.hashCode() ^ referenceWhitepoint.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Linear<Luminance<%s>>", referenceWhitepoint);
+  }
+}
